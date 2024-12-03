7d17168a42441b2ce2226d4ac41b5db9ad728b15
Riedle112 <91356940+Riedle112@users.noreply.github.com>
2024-12-03 17:53:55 +0800

    Add files via upload
    Print basic Big CODE

---
+++ finky_code.py
@@ -0,0 +1,26 @@
+#print code in large letters, vertically.
+
+char = "#" #Defining variable char == #
+print(f" {char*4}")
+print(f" {char}")
+print(f" {char}")
+print(f" {char}")
+print(f" {char*4} \n")
+
+print(f" {char*4}")
+print(f" {char}",    f" {char}")
+print(f" {char}",    f" {char}")
+print(f" {char}",    f" {char}")
+print(f" {char*4}\n")
+
+print(f" {char*3}")
+print(f" {char}",  f" {char}")
+print(f" {char}",   f" {char}")
+print(f" {char}",  f" {char}")
+print(f" {char*3} \n")
+
+print(f" {char*5}")
+print(f" {char}")
+print(f" {char*4}")
+print(f" {char}")
+print(f" {char*5}")
